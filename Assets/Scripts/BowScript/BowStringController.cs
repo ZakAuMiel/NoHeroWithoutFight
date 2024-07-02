3c2057504a4b71c0b65c04eb2c79b3489578c26d
zakaria oubbea <zakaria.oubbea@epitech.eu>
2024-07-02 20:42:20 +0200

    bow line renderer done

--- Assets/Scripts/BowScript/BowStringController.cs
+++ Assets/Scripts/BowScript/BowStringController.cs
@@ -13,17 +13,15 @@ public class BowStringController : MonoBehaviour
     private XRGrabInteractable interactable;
 
     [SerializeField]
-    private Transform midPointGrabObject, midPointVisualObject, midPointParent;
+    private Transform midPointGrabObject;
 
     private Transform interactor;
-    private float bowStringStretchLimit;
-
     private void Awake()
     {
         interactable = midPointGrabObject.GetComponent<XRGrabInteractable>();
     }
 
-    private void Start()
+    private  void Start()
     {
         interactable.selectEntered.AddListener(PrepareBowString);
         interactable.selectExited.AddListener(ResetBowString);
@@ -33,9 +31,7 @@ public class BowStringController : MonoBehaviour
     {
         interactor = null;
         midPointGrabObject.localPosition = Vector3.zero;
-        midPointVisualObject.localPosition = Vector3.zero;
         bowStringRenderer.CreateString(null);
-
     }
 
     private void PrepareBowString(SelectEnterEventArgs arg0)
@@ -47,47 +43,7 @@ public class BowStringController : MonoBehaviour
     {
         if (interactor != null)
         {
-            //convert bow string mid point position to the local space of the MidPoint
-            Vector3 midPointLocalSpace = 
-                midPointParent.InverseTransformPoint(midPointGrabObject.position); // localPosition
-
-            //get the offset
-            float midPointLocalZAbs = Mathf.Abs(midPointLocalSpace.z);
-
-            HandleStringPushedBackToStart(midPointLocalSpace);
-
-            HandleStringPulledBackTolimit(midPointLocalZAbs, midPointLocalSpace);
-
-            HandlePullingString(midPointLocalZAbs, midPointLocalSpace);
-
-            bowStringRenderer.CreateString(midPointVisualObject.position);
-        }
-    }
-
-    private void HandlePullingString(float midPointLocalZAbs, Vector3 midPointLocalSpace)
-    {
-        //what happens when we are between point 0 and the string pull limit
-        if (midPointLocalSpace.z < 0 && midPointLocalZAbs < bowStringStretchLimit)
-        {
-            midPointVisualObject.localPosition = new Vector3(0, 0, midPointLocalSpace.z);
-        }
-    }
-
-    private void HandleStringPulledBackTolimit(float midPointLocalZAbs, Vector3 midPointLocalSpace)
-    {
-        //We specify max pulling limit for the string. We don't allow the string to go any farther than "bowStringStretchLimit"
-        if (midPointLocalSpace.z < 0 && midPointLocalZAbs >= bowStringStretchLimit)
-        {
-            //Vector3 direction = midPointParent.TransformDirection(new Vector3(0, 0, midPointLocalSpace.z));
-            midPointVisualObject.localPosition = new Vector3(0, 0, -bowStringStretchLimit);
-        }
-    }
-
-    private void HandleStringPushedBackToStart(Vector3 midPointLocalSpace)
-    {
-        if (midPointLocalSpace.z >= 0)
-        {
-            midPointVisualObject.localPosition = Vector3.zero;
+            bowStringRenderer.CreateString(midPointGrabObject.transform.position);
         }
     }
 }
