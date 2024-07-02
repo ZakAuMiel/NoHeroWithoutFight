27778d373c2f549d266cf5573656224f5c746e89
zakaria oubbea <zakaria.oubbea@epitech.eu>
2024-07-02 19:42:02 +0200

    Bow start

--- Assets/Scripts/11 Wrist UI/ShotCounter.cs
+++ Assets/Scripts/11 Wrist UI/ShotCounter.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 using TMPro;
 public class ShotCounter : MonoBehaviour
 {
-    private int _shotCounter = 0;
+    
     private TextMeshProUGUI _text;
     // Start is called before the first frame update
     void Start()
     {
         _text = GetComponent<TextMeshProUGUI>();
-        FireBullet.GunFired += IncreaseCounter;
+       
         UpdateText();
     }
     private void OnDestroy()
     {
-        FireBullet.GunFired -= IncreaseCounter;
+        
     }
 
     public void ResetCounter()
     {
-        _shotCounter = 0;
+       
         UpdateText();
     }
 
     private void IncreaseCounter()
     {
-        _shotCounter++;
+        
         UpdateText();
     }
 
     private void UpdateText()
     {
-        _text.text = _shotCounter.ToString();
+       
     }
 }
